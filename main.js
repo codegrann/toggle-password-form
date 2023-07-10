c2d9091cd6c8147003a59d3f58020e0ad8de7fd4
codegrann <omondihills02@gmail.com>
2023-07-10 15:28:09 +0300

    .

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ let password = document.querySelector("#password");
 let togglePassword = document.querySelector("#togglePassword");
 let buttons=document.querySelectorAll('.buttons');
 
-document.querySelector('#signup').style.opacity=0.7
+document.querySelector('#signup').style.opacity=0.7;
 
+// attaching event listeners
 togglePassword.addEventListener("click", showPassword);
 form.addEventListener('submit', preventSubmit);
 buttons.forEach((button)=>{
     button.addEventListener('click', changeForm)
 })
 
+// toggle between login  and sign up forms
 function changeForm(e){
     let btnText=e.target.textContent;
     let submitBtn=document.querySelector('#btn-login');
     let forgetBtn=document.querySelector('#btn-forget');
+    let confirmPassword=password.nextElementSibling;
 
     submitBtn.textContent=btnText=='Sign up'? 'Sign up': 'Login';
     forgetBtn.style.display=btnText=='Sign up'? 'none': 'block';
@@ -22,9 +25,13 @@ function changeForm(e){
     buttons[1].style.opacity=btnText=='Sign up'? 1.0: 0.7;
 }
 
+// prevent form from submitting
 function preventSubmit(e){
     e.preventDefault();
+    alert("We've sent an activation link to your email")
 }
+
+// toggle visibility of password
 function showPassword() {
   let type = password.getAttribute("type");
   let newAttribute = type == "password" ? "text" : "password";
