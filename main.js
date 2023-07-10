082314277cb7826592ab0179691cd8294b26fe9e
codegrann <omondihills02@gmail.com>
2023-07-10 10:10:54 +0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+let form=document.querySelector('form');
 let password = document.querySelector("#password");
 let togglePassword = document.querySelector("#togglePassword");
 
-togglePassword.addEventListener("click", show);
+togglePassword.addEventListener("click", showPassword);
+form.addEventListener('submit', preventSubmit)
 
-function show() {
+function preventSubmit(e){
+    e.preventDefault();
+}
+function showPassword() {
   let type = password.getAttribute("type");
   let newAttribute = type == "password" ? "text" : "password";
 
