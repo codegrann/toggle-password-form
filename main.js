73a761ec5caf817d04a0bd9952dbbaf060884665
codegrann <omondihills02@gmail.com>
2023-07-10 09:56:58 +0300

    .

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-let form = document.querySelector("body");
 let password = document.querySelector("#password");
 let togglePassword = document.querySelector("#togglePassword");
-// form.style.color='red';
 
 togglePassword.addEventListener("click", show);
 
 function show() {
-//   let passwordText = password.value;
   let type = password.getAttribute("type");
   let newAttribute = type == "password" ? "text" : "password";
+
   togglePassword.classList.toggle("bi-eye");
-  
   password.setAttribute("type", newAttribute);
 }
