339c826232dd9765c53333152cd3dee9cb517327
codegrann <omondihills02@gmail.com>
2023-07-14 06:37:44 +0300

    .

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ let set = new Set();
 let john = { name: "John" };
 let pete = { name: "Pete" };
 let mary = { name: "Mary" };
+let james=Object.assign({}, john);
 
 // visits, some users come multiple times
 set.add(john);
@@ -62,10 +63,12 @@ set.add(pete);
 set.add(mary);
 set.add(john);
 set.add(mary);
+set.add(james);
 
 // set keeps only unique values
-alert( set.size ); // 3
+// alert( set.size ); // 3
 
 for (let user of set) {
-  alert(user.name); // John (then Pete and Mary)
+    console.log(user.name)
+//   alert(user.name); // John (then Pete and Mary)
 }
