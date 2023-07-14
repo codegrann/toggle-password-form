dc6cef1438a90f72e8eb0adf606c46fd1ebd1dd6
codegrann <omondihills02@gmail.com>
2023-07-14 06:45:24 +0300

    .

--- main.js
+++ main.js
@@ -48,12 +48,3 @@ function showPassword() {
   togglePassword.classList.toggle("bi-eye");
   password.setAttribute("type", newAttribute);
 }
-
-
-let john = { name: "John" };
-
-let map = new Map();
-map.set(john, "...");
-console.log(map)
-john = null;
-console.log(map)
