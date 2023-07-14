236b411e379cf47bd1fbce6889dd87440f68e31e
codegrann <omondihills02@gmail.com>
2023-07-14 06:38:00 +0300

    .

--- main.js
+++ main.js
@@ -48,27 +48,3 @@ function showPassword() {
   togglePassword.classList.toggle("bi-eye");
   password.setAttribute("type", newAttribute);
 }
-
-
-let set = new Set();
-
-let john = { name: "John" };
-let pete = { name: "Pete" };
-let mary = { name: "Mary" };
-let james=Object.assign({}, john);
-
-// visits, some users come multiple times
-set.add(john);
-set.add(pete);
-set.add(mary);
-set.add(john);
-set.add(mary);
-set.add(james);
-
-// set keeps only unique values
-// alert( set.size ); // 3
-
-for (let user of set) {
-    console.log(user.name)
-//   alert(user.name); // John (then Pete and Mary)
-}
